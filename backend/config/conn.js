78b11dbff97c0d78602a5a8ee32d3d7e99420f0d
ashok-kumar2004 <ashokkumar2482004@gmail.com>
2025-07-08 23:15:08 +0530

    Reorganized project structure and updated .gitignore

--- backend/config/conn.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const mongoose=require('mongoose');
-
-const dbconnect=()=>{mongoose.connect(process.env.MONGODB_URL).then(()=>{
-    console.log("DB Coonected successfully")
-}).catch((err)=>{console.log(err)})}
-module.exports=dbconnect;
